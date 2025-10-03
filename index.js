b3e90f82857825adf461f588d6f234f370d675a7
joshwizard <murikij@gmail.com>
2025-10-04 00:06:49 +0300

    Added data

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Add your code here
+// // Add your code here
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json()
+//     })
+//     .then(function (object) {
+//         console.log(object)
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Check your code")
+//         console.log(error.message)
+//     })
+
+
+
+function submitData(name, email) {
+    return fetch ("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify( {
+            name: name,
+            email: email,
+        })
+    })
+
+    .then(function (response) {
+        return response.json()
+    })
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
